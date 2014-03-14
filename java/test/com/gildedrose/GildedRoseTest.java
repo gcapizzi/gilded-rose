7ee95181cda70cf6ccee9571f7805278ac546382
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-03-14 16:21:45 +0100

    Move tests to the test directory

---
+++ java/test/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,13 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GildedRoseTest {
+
+    @Test
+    public void testUpdateQuality() {
+        assertTrue(true);
+    }
+}
