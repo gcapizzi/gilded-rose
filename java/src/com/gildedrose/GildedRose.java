3e2ec28facc3c0ade1dde4635a42bbea6bb3e764
Giuseppe Capizzi <g.capizzi@gmail.com>
2014-03-16 14:32:42 +0100

    Streamline logic

--- java/src/com/gildedrose/GildedRose.java
+++ java/src/com/gildedrose/GildedRose.java
@@ -9,30 +9,19 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.isAgedBrie() && !item.isBackstagePasses()) {
-                item.decrementQuality();
-            } else {
-                item.incrementQuality();
-
-                if (item.isBackstagePasses()) {
-                    if (item.sellIn < 11) {
-                        item.incrementQuality();
-                    }
-
-                    if (item.sellIn < 6) {
-                        item.incrementQuality();
-                    }
-                }
-            }
-
             item.decrementSellIn();
 
-            if (item.sellIn < 0) {
-                if (!item.isAgedBrie()) {
-                    item.decrementQuality();
-                } else {
-                    item.incrementQuality();
-                }
+            if (item.isAgedBrie()) {
+                item.incrementQuality();
+                if (item.sellIn < 0) { item.incrementQuality(); }
+            } else if (item.isBackstagePasses()) {
+                item.incrementQuality();
+                if (item.sellIn < 11) { item.incrementQuality(); }
+                if (item.sellIn < 6)  { item.incrementQuality(); }
+                if (item.sellIn < 0)  { item.decrementQuality(); }
+            } else {
+                item.decrementQuality();
+                if (item.sellIn < 0) { item.decrementQuality(); }
             }
         }
     }
